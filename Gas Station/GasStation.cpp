927d3a330f574dc078af2079134ffcf84586b6eb
kennz2410 <kennz2410@gmail.com>
2025-04-22 21:20:20 +0700

    New Update

--- Gas Station/GasStation.cpp
+++ Gas Station/GasStation.cpp
@@ -3,27 +3,11 @@
 using namespace std;
 
 int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-    int currGas = 0;
-    int index = 0;
-    int totalTank = 0;
-    for(int i = 0; i < gas.size(); i++) {
-        currGas += gas[i] - cost[i];
-        totalTank += gas[i] - cost[i];
-        if(currGas < 0)  {
-            index = i + 1;
-            currGas = 0;
-        };
-    }
-
-    return (totalTank >= 0) ? index : -1;
+        
 }
 
 int main() {
-    vector<int> gas = {2, 3, 4};
-    vector<int> cost = {3, 4, 3};
+    vector<int> gas = {1, 2, 3, 4, 5};
+    vector<int> cost = {3, 4, 5, 1, 2};
     int result = canCompleteCircuit(gas, cost);
-
-    cout << result << endl;
-
-    return 0;
 }
